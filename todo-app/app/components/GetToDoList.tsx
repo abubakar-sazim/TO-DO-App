f371bafd74b6d4bd0a3422c54fa48e8ecee3ffe7
Bakar31 <abubakar.siddik@sazim.io>
2023-11-04 17:00:40 +0600

    resolved issues mentioned in PR comments

--- todo-app/app/components/GetToDoList.tsx
+++ todo-app/app/components/GetToDoList.tsx
@@ -1,36 +1,33 @@
 async function GetTasks() {
-    const res = await fetch('http://localhost:8000/tasks', {
+    const res = await fetch("http://localhost:8000/tasks", {
         next: {
-        revalidate: 0 // use 0 to opt out of using cache
-        }
-    })
+        revalidate: 0, // use 0 to opt out of using cache
+        },
+    });
 
-    return res.json()
+    return res.json();
 }
 
-export default async function ToDoList(){
-    const tasks = await GetTasks()
-    // console.log(tasks)
+export default async function ToDoList() {
+    const tasks = await GetTasks();
     return (
         <div className="overflow-x-auto">
-            <table className="table">
-            {/* head */}
+        <table className="table">
             <thead>
-                <tr>
+            <tr>
                 <th>Tasks</th>
                 <th>Actions</th>
-                </tr>
+            </tr>
             </thead>
             <tbody>
-                {tasks.map(todotask => (
-                    <tr key={todotask.id}>
-                    <td>{todotask.text}</td>
-                    <td>Pending</td>
-                    </tr>
-                ))}
-                
+            {tasks.map((todotask) => (
+                <tr key={todotask.id}>
+                <td>{todotask.text}</td>
+                <td>Pending</td>
+                </tr>
+            ))}
             </tbody>
-            </table>
-            </div>
+        </table>
+        </div>
     );
 }
--- todo-app/app/page.tsx
+++ todo-app/app/page.tsx
@@ -6,7 +6,6 @@ export default async function Home() {
       <main className='max-w-4xl mx-auto mt-4'>
         <div className='text-center my-5 flex-column gap-4'>
           <h1 className='text-2xl font-bold text-emerald-600'>TO DO APP</h1>
-          {/* <AddTask /> */}
           <ToDoList/>
           <div>
           </div>
