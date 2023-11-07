715cf0abdbdf2060ed91db38197867b905ce32c0
Bakar31 <abubakar.siddik@sazim.io>
2023-11-07 18:30:31 +0600

    format correction

--- todo-app/app/page.tsx
+++ todo-app/app/page.tsx
@@ -2,23 +2,18 @@ import { GetTasks } from "@/api";
 import AddTask from "./components/AddTask";
 import ToDoList from "./components/GetToDoList";
 
-
 export default async function Home() {
-
   const tasks = await GetTasks();
 
   return (
-      <main className='max-w-4xl mx-auto mt-4'>
-        <div className='text-center my-5 flex-column gap-4'>
-          <h1 className='text-2xl font-bold text-emerald-600'>TO DO APP</h1>
-          <AddTask />
-          <div>
+    <main className="max-w-4xl mx-auto mt-4">
+      <div className="text-center my-5 flex-column gap-4">
+        <h1 className="text-2xl font-bold text-emerald-600">TO DO APP</h1>
+        <AddTask />
+        <div>
           <ToDoList tasks={tasks} />
-          </div>
-          <div>
-          </div>
         </div>
-      </main>
-  )
+      </div>
+    </main>
+  );
 }
-
