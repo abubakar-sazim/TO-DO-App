466ca5f4ff0cc3796afd1e05207836914737844d
Bakar31 <abubakar.siddik@sazim.io>
2023-11-07 17:37:30 +0600

    drag and drop feature

--- todo-app/app/components/GetToDoList.tsx
+++ todo-app/app/components/GetToDoList.tsx
@@ -1,29 +1,49 @@
+"use client";
+
 import { ToDoTask } from "@/types/task";
 import Task from "./Task";
+import React from "react";
 
 interface TodoListProps {
     tasks: ToDoTask[];
     }
 
 const TodoList: React.FC<TodoListProps> = ({ tasks }) => {
-    return (
-        <div className="overflow-x-auto">
-            <table className="table">
-            <thead>
-                <tr>
-                <th>Tasks</th>
-                <th>Actions</th>
-                <th>State</th>
-                </tr>
-            </thead>
+  const [todotasks, setTodoTasks] = React.useState([tasks]);
+  const dragItem = React.useRef<any>(null);
+  const dragOverItem = React.useRef<any>(null);
+
+  const handleSort = () => {
+    let _todotasks = [...todotasks];
+    const draggedItemContent = _todotasks[0].splice(dragItem.current, 1)[0];
+    _todotasks[0].splice(dragOverItem.current, 0, draggedItemContent);
+
+    dragItem.current = null;
+    dragOverItem.current = null;
+    setTodoTasks(_todotasks);
+  };
+
+  return (
+    <div className="overflow-x-auto">
+      {tasks.map((task, index) => (
+        <div
+          className="font-bold text-lg border-spacing-3 border-s-success-content"
+          key={index}
+          draggable
+          onDragStart={(e) => (dragItem.current = index)}
+          onDragEnter={(e) => (dragOverItem.current = index)}
+          onDragEnd={handleSort}
+          onDragOver={(e) => e.preventDefault()}
+        >
+          <table className="table">
             <tbody>
-                {tasks.map((task) => (
-                <Task key={task.id} task={task} />
-                ))}
+              <Task key={task.id} task={task} />
             </tbody>
-            </table>
-            </div>
-    );
-}
+          </table>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default TodoList;
